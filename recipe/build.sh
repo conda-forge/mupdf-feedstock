ac849c0994338a1fc15a7f2f29d60a29ab84e954
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-29 12:58:05 -0500

    make all c++ python

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,6 +21,6 @@ export USE_SYSTEM_JPEGXR=yes
 #ls -lh ${PREFIX}/lib
 
 # build and install
-make prefix="${PREFIX}" -j ${CPU_COUNT} all
+make prefix="${PREFIX}" -j ${CPU_COUNT} all c++ python
 # no make check
 make prefix="${PREFIX}" install
