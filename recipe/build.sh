b3434f00ffabc5962e5bf7f9b705d13d18cefc3e
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-29 13:54:01 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,6 +23,9 @@ export USE_SYSTEM_JPEGXR=yes
 # build and install
 make prefix="${PREFIX}" -j ${CPU_COUNT} all
 
+# no make check
+make prefix="${PREFIX}" install
+
 # build c++ bindings
 ./scripts/mupdfwrap.py -d build/shared-release -b 01
 
