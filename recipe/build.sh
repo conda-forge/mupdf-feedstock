248e029fa9fdb0013d16d56bc9cfe5682ec68043
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-06 19:58:37 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,15 @@ set -ex
 # build system uses non-standard env vars
 uname=$(uname)
 if [[ "$target_platform" == osx* ]]; then
+  export OS="MACOS"
   #export LIBS="${LIBS} -L${PREFIX}/lib -v"
   #export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -v"
   export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2"
   export CFLAGS="${CFLAGS} -I $(ls -d ${PREFIX}/include/openjpeg-*)"
   #export SYS_FREETYPE_LIBS=" -lfreetype"
   #export SYS_FREETYPE_CFLAGS="${CFLAGS}"
+else:
+  export OS="Linux"
 fi
 export CFLAGS="${CFLAGS} -I ${PREFIX}/include/harfbuzz"
 export XCFLAGS="${CFLAGS}"
