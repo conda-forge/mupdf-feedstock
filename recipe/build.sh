c9e0edcb11678072e0e24c7a685e0add4dc51c86
Jonas Haag <github@jonashaag.de>
2025-01-07 14:56:44 +0100

    osx-arm64 (#68)
    * wip
    
    * Update conda-forge.yml
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.0, and conda-forge-pinning 2024.12.18.11.57.07
    
    * Update build.sh
    
    * wip
    
    * Fix typo and add c_stdlib_version configuration
    
    * halp
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.0, and conda-forge-pinning 2024.12.18.11.57.07
    
    * Fix typo in conda_build_config.yaml
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2024.12.29.20.31.45
    
    * Update macOS SDK and c_stdlib versions
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2024.12.29.20.31.45
    
    * Update macOS SDK and c_stdlib versions
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2024.12.29.20.31.45
    
    * Update macOS SDK and c_stdlib versions
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2024.12.29.20.31.45
    
    * Update macOS SDK and c_stdlib versions
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2024.12.29.20.31.45
    
    * Update build.sh
    
    * fix
    
    * Move CFLAGS export outside osx-arm64 condition
    
    * try older sdk
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2025.01.07.09.05.09
    
    * Delete recipe/conda_build_config.yaml
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,11 +8,15 @@ if [[ "$target_platform" == osx* ]]; then
   #export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -v"
   export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2"
   export CFLAGS="${CFLAGS} -I $(ls -d ${PREFIX}/include/openjpeg-*)"
+  export CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
   #export SYS_FREETYPE_LIBS=" -lfreetype"
   #export SYS_FREETYPE_CFLAGS="${CFLAGS}"
   export TESSERACT=false
 else
   export TESSERACT=yes
+fi
+if [[ "$target_platform" == osx-arm64 ]]; then
+  export ARCHFLAGS="-arch arm64"
 fi
 export CFLAGS="${CFLAGS} -I ${PREFIX}/include/harfbuzz"
 export XCFLAGS="${CFLAGS}"
